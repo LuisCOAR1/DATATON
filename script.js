7b414074302277c3be2d1994c5a7de46a545b0ed
LuisCOAR1 <61335831@cusco.coar.edu.pe>
2023-11-26 09:16:27 -0500

    Se añade los cambios

--- script.js
+++ script.js
@@ -9,3 +9,45 @@ function iniciarMap(){
       map: map
     });
 }
+var publicSectorBtn = document.getElementById("public_sector");
+var juridicSectorBtn = document.getElementById("juridic_sector");
+var educationSectorBtn = document.getElementById("education_sector");
+var saludSectorBtn = document.getElementById("salud_sector");
+var bloque1 = document.getElementById("bloque1");
+var bloque2 = document.getElementById("bloque2");
+var bloque3 = document.getElementById("bloque3");
+var bloque4 = document.getElementById("bloque4");
+
+function mostrarBloque1() {
+  bloque1.style.display = "flex";
+  bloque2.style.display = "none";
+  bloque3.style.display = "none"
+  bloque4.style.display = "none";
+}
+mostrarBloque1()
+
+function mostrarBloque2() {
+  bloque3.style.display = "none"
+  bloque2.style.display = "flex";
+  bloque1.style.display = "none";
+  bloque4.style.display = "none";
+}
+
+function mostrarBloque3() {
+  bloque3.style.display = "flex"
+  bloque2.style.display = "none";
+  bloque1.style.display = "none";
+  bloque4.style.display = "none";
+}
+
+function mostrarBloque4() {
+  bloque4.style.display = "flex";
+  bloque3.style.display = "none";
+  bloque2.style.display = "none";
+  bloque1.style.display = "none";
+}
+
+publicSectorBtn.addEventListener("click", mostrarBloque1);
+juridicSectorBtn.addEventListener("click", mostrarBloque2);
+educationSectorBtn.addEventListener("click", mostrarBloque3);
+saludSectorBtn.addEventListener("click", mostrarBloque4);
